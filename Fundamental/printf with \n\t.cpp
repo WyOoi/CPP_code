9d16e8e38f29217160aca19e7cc8238aad5fcf19
Weiyuan <128281190+WyOoi@users.noreply.github.com>
2024-11-14 20:39:18 +0800

    Create printf with \n\t.cpp

---
+++ Fundamental/printf with \n\t.cpp
@@ -0,0 +1,9 @@
+/*Weiyuan_Practical Excercise 1*/
+
+#include<stdio.h>
+
+int main()
+{ 
+    printf("\t Name:Ooi Wei Yuan \n \t Registration Number:18DTK21F1075 \n \t IC Number:030109-02-0011 \n \t Phone Number:018471500 \n \t Address:486 TAMAN SRI KOTA JALAN KUAA KEDAH 06600 ALOR SETAR KEDAH");
+    return 0 ;
+}
