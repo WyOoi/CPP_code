6bf39a49d12d06d0b01637fde07890effad53936
Weiyuan <128281190+WyOoi@users.noreply.github.com>
2024-11-14 20:35:10 +0800

    Create ForLoop.cpp

---
+++ Fundamental/ForLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//    for(int i=2;i<20;i=i+2){
+//    	cout << i << endl;
+//    	cout << "\n";
+//	}
+	for(int i=49;i>0;i=i-2){
+    	cout << i << endl;
+//    	cout << "\n";
+	}
+    return 0;
+}
